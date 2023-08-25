c9c9056bb2e9326c3e1399cc1dc84d48d444919f
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-08-24 23:28:03 -0700

    implemented get routes and axios

--- backend/src/routes/api/unemploymentRate.ts
+++ backend/src/routes/api/unemploymentRate.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from 'express';
+import axios from 'axios';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const UnemploymentRateRoute = express.Router();
 
-UnemploymentRateRoute.get('/', (req: Request, res: Response): void => {
-  res.status(200).json({
-    "Unemployment Rate": "test response"
-  })
+UnemploymentRateRoute.get('/', async (req: Request, res: Response) => {
+  try {
+    const unemploymentData = await axios.get(`https://api.stlouisfed.org/fred/series/observations?series_id=DGS10&file_type=json&api_key=${process.env.FRED_API_KEY}`)
+    res.json({
+      "Unemployment Rate": unemploymentData.data.observations
+    });
+  } catch (e) {
+    console.error(e);
+  }
 });
