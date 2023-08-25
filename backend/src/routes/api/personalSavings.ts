f85fc58aa09c73e162831c44284f1fcaca045803
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-08-24 23:23:18 -0700

    implemented get routes and axios

--- backend/src/routes/api/personalSavings.ts
+++ backend/src/routes/api/personalSavings.ts
@@ -7,8 +7,8 @@ dotenv.config();
 export const personalSavingsRoute = express.Router();
 
 personalSavingsRoute.get('/', async (req: Request, res: Response) => {
-  const personalSavingsData = await axios.get(`https://api.stlouisfed.org/fred/series/observations?series_id=JTSTSR&file_type=json&api_key=${process.env.FRED_API_KEY}`)
+  const personalSavingsData = await axios.get(`https://api.stlouisfed.org/fred/series/observations?series_id=PSAVERT&file_type=json&api_key=${process.env.FRED_API_KEY}`)
   res.json({
-    "Personal Consumption": personalSavingsData.data.observations
+    "Personal Savings": personalSavingsData.data.observations
   });
 });
