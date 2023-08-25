fdef8b925a30c4b14ac83bee7b9691f13dd58492
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-08-24 23:13:00 -0700

    implemented get routes and axios

--- backend/src/routes/api/consumerPriceIndex.ts
+++ backend/src/routes/api/consumerPriceIndex.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from 'express';
+import axios from 'axios';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const consumerPriceIndexRoute = express.Router();
 
-consumerPriceIndexRoute.get('/', (req: Request, res: Response): void => {
-  res.status(200).json({
-    "Consumer Price Index": "test response"
-  })
+consumerPriceIndexRoute.get('/', async (req: Request, res: Response) => {
+  const cpiData = await axios.get(`https://api.stlouisfed.org/fred/series/observations?series_id=CPIAUCSL&file_type=json&api_key=${process.env.FRED_API_KEY}`)
+  res.json({
+    "Consumer Price Index": cpiData.data.observations
+  });
 });
--- backend/src/routes/api/fedFundsRate.ts
+++ backend/src/routes/api/fedFundsRate.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from 'express';
+import axios from 'axios';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const fedFundsRoute = express.Router();
 
-fedFundsRoute.get('/', (req: Request, res: Response): void => {
-  res.status(200).json({
-    "ffr": "test response"
-  })
+fedFundsRoute.get('/', async (req: Request, res: Response) => {
+  const fedFundsRateData = await axios.get(`https://api.stlouisfed.org/fred/series/observations?series_id=EFFR&file_type=json&api_key=${process.env.FRED_API_KEY}`)
+  res.json({
+    "Federal Funds Rate": fedFundsRateData.data.observations
+  });
 });
