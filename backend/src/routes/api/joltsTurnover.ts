d99e0878d03d4371fdaa3a63d2b23d7920541df6
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-08-24 23:33:43 -0700

    implemented get routes and axios

--- backend/src/routes/api/joltsTurnover.ts
+++ backend/src/routes/api/joltsTurnover.ts
@@ -7,8 +7,12 @@ dotenv.config();
 export const joltsTurnoverRoute = express.Router();
 
 joltsTurnoverRoute.get('/', async (req: Request, res: Response) => {
-  const joltsTurnoverData = await axios.get(`https://api.stlouisfed.org/fred/series/observations?series_id=JTSTSR&file_type=json&api_key=${process.env.FRED_API_KEY}`)
-  res.json({
-    "JOLTS Turnover": joltsTurnoverData.data.observations
-  });
+  try {
+    const joltsTurnoverData = await axios.get(`https://api.stlouisfed.org/fred/series/observations?series_id=JTSTSR&file_type=json&api_key=${process.env.FRED_API_KEY}`)
+    res.json({
+      "JOLTS Turnover": joltsTurnoverData.data.observations
+    });
+  } catch (e) {
+    console.error(e);
+  }
 });
--- backend/src/routes/api/personalSavings.ts
+++ backend/src/routes/api/personalSavings.ts
@@ -7,8 +7,12 @@ dotenv.config();
 export const personalSavingsRoute = express.Router();
 
 personalSavingsRoute.get('/', async (req: Request, res: Response) => {
-  const personalSavingsData = await axios.get(`https://api.stlouisfed.org/fred/series/observations?series_id=PSAVERT&file_type=json&api_key=${process.env.FRED_API_KEY}`)
-  res.json({
-    "Personal Savings": personalSavingsData.data.observations
-  });
+  try {
+    const personalSavingsData = await axios.get(`https://api.stlouisfed.org/fred/series/observations?series_id=PSAVERT&file_type=json&api_key=${process.env.FRED_API_KEY}`)
+    res.json({
+      "Personal Savings": personalSavingsData.data.observations
+    });
+  } catch (e) {
+    console.error(e);
+  }
 });
