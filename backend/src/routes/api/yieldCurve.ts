d17afd6d865a0ee39ac26de4cf098e6ea38943e9
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2023-08-24 23:52:57 -0700

    updated tsconfig `target`

--- backend/src/routes/api/yieldCurve.ts
+++ backend/src/routes/api/yieldCurve.ts
@@ -6,11 +6,16 @@ dotenv.config();
 
 export const yieldCurveRoute = express.Router();
 
+function FRED_data_formatter(data) {
+  console.log(data)
+}
+
 yieldCurveRoute.get('/', async (req: Request, res: Response) => {
   try {
-    const yieldCurveDate = await axios.get(`https://api.stlouisfed.org/fred/series/observations?series_id=DGS10&file_type=json&api_key=${process.env.FRED_API_KEY}`)
+    const yieldCurveData = await axios.get(`https://api.stlouisfed.org/fred/series/observations?series_id=DGS10&file_type=json&api_key=${process.env.FRED_API_KEY}`)
+    FRED_data_formatter(yieldCurveData)
     res.json({
-      "Yield Curve": yieldCurveDate.data.observations
+      "Yield Curve": yieldCurveData.data.observations
     });
   } catch (e) {
     console.error(e);
